d453ecd536131e6a6130c8fc70b11a4b23544f8c
rodrigoorozco <rodrigoorozco025@gmail.com>
2024-06-30 22:47:55 -0500

    triangle rasterization

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 #include "vendor/model/model.h"
 #include "vendor/model/geometry.h"
 
+#include <vector>
+#include <iostream>
+
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-const int width = 800;
-const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 {
@@ -34,27 +35,76 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 }
 
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
+{
+	if (t0.y > t1.y)
+	 	std::swap(t0, t1);
+	if (t0.y > t2.y)
+		std::swap(t0, t2);
+	if (t1.y > t2.y)
+		std::swap(t1, t2);
+	
+	int t_height = t2.y - t0.y;
+	for (int y=t0.y; y<=t1.y; y++)
+	{
+		int segment_height = t1.y - t0.y + 1;
+		float alpha = (float) (y-t0.y) / t_height;
+		float beta = (float) (y-t0.y) / segment_height; // handle division by zero
+		Vec2i A = t0 + (t2-t0)*alpha;
+		Vec2i B = t0 + (t1-t0)*beta;
+		if (A.x > B.x)
+			std::swap(A, B);
+		for (int j=A.x; j<=B.x; j++)
+		{
+			image.set(j, y, color);
+		}
+	}
+	for (int y=t1.y; y<=t2.y; y++)
+	{
+		int segment_height = t2.y - t1.y + 1;
+		float alpha = (float) (y-t0.y) / t_height;
+		float beta = (float) (y-t1.y) / segment_height; // handle division by zero
+		Vec2i A = t0 + (t2-t0)*alpha;
+		Vec2i B = t1 + (t2-t1)*beta;
+		if (A.x > B.x)
+			std::swap(A, B);
+		for (int j=A.x; j<=B.x; j++)
+		{
+			image.set(j, y, color);
+		}
+	}
+
+}
+
 int main(int argc, char** argv) 
 {
-	// Bresenham's line drawing unoptomized obj demo :
+	const int width = 800;
+	const int height = 800;
+	
+	// Traingle rasterization demo:
 	Model* model = new Model("../res/obj/african_head.obj");
 	
     TGAImage image(width, height, TGAImage::RGB);
+    Vec3f light_dir(0,0,-1);
     for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
         for (int j=0; j<3; j++) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j+1)%3]);
-            int x0 = (v0.x+1.)*width/2.;
-            int y0 = (v0.y+1.)*height/2.;
-            int x1 = (v1.x+1.)*width/2.;
-            int y1 = (v1.y+1.)*height/2.;
-            line(x0, y0, x1, y1, image, white);
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
+            world_coords[j]  = v;
+        }
+        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+        n.normalize();
+        float intensity = n*light_dir;
+        if (intensity>0) {
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
         }
     }
 
 	image.flip_vertically(); // origin at the left bottom corner of the image
-	image.write_tga_file("bresenhams_line_draw.tga");
+	image.write_tga_file("triangle_rasterization.tga");
 	delete model;
 	return 0;
 }
